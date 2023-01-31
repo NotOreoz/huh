7879510af84dd260cafdd2ef0964b3127a60c545
NotOreoz <112834026+NotOreoz@users.noreply.github.com>
2023-01-30 22:05:45 -0600

    Update bob.lua

--- bob.lua
+++ bob.lua
@@ -7,6 +7,7 @@ local ESP = {
     Color = Color3.fromRGB(255, 170, 0),
     FaceCamera = false,
     Names = true,
+    ShowDistance = false,
     TeamColor = true,
     Thickness = 2,
     AttachShift = 1,
@@ -228,7 +229,7 @@ function boxBase:Update()
             self.Components.Name.Text = self.Name
             self.Components.Name.Color = color
             
-            self.Components.Distance.Visible = false
+            self.Components.Distance.Visible = ESP.ShowDistance
             self.Components.Distance.Position = Vector2.new(TagPos.X, TagPos.Y + 14)
             self.Components.Distance.Text = math.floor((cam.CFrame.p - cf.p).magnitude) .."m away"
             self.Components.Distance.Color = color
